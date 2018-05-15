df18efd1ad3f56bb4d08c2fc8daace6aa72d5197
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-05-15 16:01:21 +0300

    feat: Rewrite sorting algorithm test using macros to make it scalable

--- src/algorithms/sorting/mod.rs
+++ src/algorithms/sorting/mod.rs
@@ -2,13 +2,22 @@ pub mod bubble;
 
 #[cfg(test)]
 mod tests {
-    use super::bubble;
+    macro_rules! sorting_test {
+        ($($name:ident: $algorithm:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let sort = $algorithm;
+                    let elements = vec![23, 4, 16, 15, 8, 42];
+                    let sorted = sort(&elements);
+                    let expected = vec![4, 8, 15, 16, 23, 42];
+                    assert_eq!(sorted, expected);
+                }
+            )*
+        }
+    }
 
-    #[test]
-    fn bubble_sort() {
-        let elements = vec![23, 4, 16, 15, 8, 42];
-        let sorted = bubble::sort(&elements);
-        let expected = vec![4, 8, 15, 16, 23, 42];
-        assert_eq!(sorted, expected);
+    sorting_test! {
+        bubble_sort: super::bubble::sort,
     }
 }
