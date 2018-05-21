ec55499174d2df6cb28eb90e8886604fb6106816
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-05-21 11:38:42 +0300

    refactor: Rename cursor to pivot and remove redundant reassignment of it

--- src/algorithms/sorting/quicksort.rs
+++ src/algorithms/sorting/quicksort.rs
@@ -9,15 +9,14 @@ fn quick_sort<T: Ord + Clone>(list: &mut Vec<T>, start: usize, end: usize) {
     if start >= end { return; }
     let mut left = start;
     let mut right = end;
-    let mut cursor = (start + end) / 2;
+    let pivot = (start + end) / 2;
     while left < right {
-        while left < cursor && list[left] <= list[cursor] { left += 1; }
-        while right > cursor && list[right] >= list[cursor] { right -= 1; }
+        while left < pivot && list[left] <= list[pivot] { left += 1; }
+        while right > pivot && list[right] >= list[pivot] { right -= 1; }
         if left < right {
             list.swap(left, right);
-            cursor = if cursor == left { left } else if cursor == right { right } else { cursor };
         }
     }
-    quick_sort(list, start, cursor);
-    quick_sort(list, cursor + 1, end);
+    quick_sort(list, start, pivot);
+    quick_sort(list, pivot + 1, end);
 }
