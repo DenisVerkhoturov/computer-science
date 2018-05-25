c83979aaa5a16b04d0f52ee2be336f6d2dea0826
Denis Verkhoturov <denis_verkhoturov@epam.com>
2018-05-25 14:22:45 +0300

    test: Add different data cases to be sure algorithms works on any data

--- src/algorithms/sorting/mod.rs
+++ src/algorithms/sorting/mod.rs
@@ -5,29 +5,61 @@ pub mod insert;
 pub mod select;
 pub mod merge;
 
-#[cfg(test)]
-mod tests {
-    macro_rules! sorting_test {
-        ($($name:ident: $algorithm:expr,)*) => {
-            $(
+macro_rules! sorting_test {
+    ($($name:ident: $algorithm:expr,)*) => {
+        $(
+            #[cfg(test)]
+            mod $name {
                 #[test]
-                fn $name() {
+                fn should_sort_sorted() {
+                    let expected = vec![4, 8, 15, 16, 23, 42];
+                    let elements = vec![4, 8, 15, 16, 23, 42];
+                    let sorted = $algorithm(&elements);
+                    assert_eq!(sorted, expected);
+                }
+
+                #[test]
+                fn should_sort_random_sorted() {
                     let expected = vec![4, 8, 15, 16, 23, 42];
                     let elements = vec![23, 4, 16, 15, 8, 42];
                     let sorted = $algorithm(&elements);
                     assert_eq!(sorted, expected);
                 }
-            )*
-        }
-    }
 
-    sorting_test! {
-        bubble_sort: super::bubble::sort,
-        gnome_sort: super::gnome::sort,
-        quick_sort: super::quicksort::sort,
-        insert_sort: super::insert::sort,
-        select_sort: super::select::sort,
-        merge_top_down_sort: super::merge::top_down::sort,
-        merge_bottom_up_sort: super::merge::bottom_up::sort,
+                #[test]
+                fn should_sort_nearly_sorted() {
+                    let expected = vec![4, 8, 15, 16, 23, 42];
+                    let elements = vec![8, 4, 15, 23, 16, 42];
+                    let sorted = $algorithm(&elements);
+                    assert_eq!(sorted, expected);
+                }
+
+                #[test]
+                fn should_sort_reversed() {
+                    let expected = vec![4, 8, 15, 16, 23, 42];
+                    let elements = vec![42, 23, 16, 15, 8, 4];
+                    let sorted = $algorithm(&elements);
+                    assert_eq!(sorted, expected);
+                }
+
+                #[test]
+                fn should_sort_few_unique() {
+                    let expected = vec![8, 8, 23, 23, 42, 42];
+                    let elements = vec![42, 23, 8, 23, 8, 42];
+                    let sorted = $algorithm(&elements);
+                    assert_eq!(sorted, expected);
+                }
+            }
+        )*
     }
+}
+
+sorting_test! {
+    bubble_sort: super::bubble::sort,
+    gnome_sort: super::gnome::sort,
+    quick_sort: super::quicksort::sort,
+    insert_sort: super::insert::sort,
+    select_sort: super::select::sort,
+    merge_top_down_sort: super::merge::top_down::sort,
+    merge_bottom_up_sort: super::merge::bottom_up::sort,
 }
